3cc394b916eb99c2f01f44079c930d0cfda6328f
yolo <igoncharov@flightpath3d.com>
2024-02-20 15:42:19 +0000

    db: games

--- src/database/db.ts
+++ src/database/db.ts
@@ -9,9 +9,11 @@ interface Winner {
 export const dbPlayer: Record<number, Player> = {};
 export const winnersDb: Winner[] = [];
 export const dbRoom: Record<number, Room> = {};
+export const dbGames: Record<number, { id: number; playerIds: number[] }> = {};
 
 export default {
   dbPlayer,
   winnersDb,
+  dbGames,
   dbRoom,
 };
