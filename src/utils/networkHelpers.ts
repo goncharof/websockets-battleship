9d56758ead57aa72d61edd9bb22215a94e46309b
yolo <igoncharov@flightpath3d.com>
2024-02-22 13:40:22 +0000

    feat: utils - networkHelpers

---
+++ src/utils/networkHelpers.ts
@@ -0,0 +1,25 @@
+import { ExtWebSocket } from "../wss";
+
+export enum WsMsgTypes {
+  Reg = "reg",
+  CreateRoom = "create_room",
+  AddUserToRoom = "add_user_to_room",
+  StartGame = "start_game",
+  UpdateWinners = "update_winners",
+  UpdateRoom = "update_room",
+  CreateGame = "create_game",
+}
+
+export const sendWsMessage = (
+  ws: ExtWebSocket,
+  type: WsMsgTypes,
+  data: object,
+) => {
+  ws.send(
+    JSON.stringify({
+      type,
+      id: 0,
+      data: JSON.stringify(data),
+    }),
+  );
+};
